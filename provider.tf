af5aadd4ccf70ca3aa86ee468cf00309ed07804c
Mangwani <gaurav.mangwani@m-fypk9hn636.communityfibre.co.uk>
2025-04-09 12:15:07 +0100

    initial commit

---
+++ provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.94.1"
+    }
+  }
+}
+provider "aws" {
+  # Configuration options
+  access_key = var.akey
+  secret_key = var.skey
+  region = var.region
+}
+terraform {
+  cloud {
+    organization = "NatwestOrg"
+    workspaces {
+      name = "P1W1"
+    }
+  }
+}
---
+++ s3.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "b1" {
+  bucket = "gauravm-test-bucket-1234567890"
+}
