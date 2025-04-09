ee62ee2367c73c071e5819254e11d11c5a77596c
Mangwani <gaurav.mangwani@m-fypk9hn636.communityfibre.co.uk>
2025-04-09 14:18:51 +0100

    updated buckets

--- s3.tf
+++ s3.tf
@@ -1,15 +1,15 @@
 resource "aws_s3_bucket" "b1" {
-  bucket = "gauravm-test-bucket-1234567890"
+  bucket = "tgauravm-test-bucket-1234567890"
 }
 
 resource "aws_s3_bucket" "b2" {
-  bucket = "gauravm-test2-bucket-1234567890"
+  bucket = "tgauravm-test2-bucket-1234567890"
 }
 
 resource "aws_s3_bucket" "b3" {
-  bucket = "gauravm-test3-bucket-1234567890"
+  bucket = "tgauravm-test3-bucket-1234567890"
 }
 
 resource "aws_s3_bucket" "b4" {
-  bucket = "gauravm4-test-bucket-1234567890"
+  bucket = "tgauravm4-test-bucket-1234567890"
 }
